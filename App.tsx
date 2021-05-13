726bd4f21fec9ecf9090197622aad5e4133075d5
NoronhaGCP <inc.691@gmail.com>
2021-05-13 17:56:28 -0300

    implement font loading

--- App.tsx
+++ App.tsx
@@ -1,21 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import {
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_700Bold
+  } from '@expo-google-fonts/roboto';
+
+import Loading from './src/utils/Loading';
+import { Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [ fontsLoaded ] = useFonts({
+      Roboto_300Light,
+      Roboto_400Regular,
+      Roboto_400Regular_Italic,
+      Roboto_500Medium,
+      Roboto_700Bold
+    });
+    
+    return (
+        <>
+          <StatusBar style="auto" />
+          {
+            !fontsLoaded 
+            ? <Loading />
+            : <Text>Hello World!</Text>
+          }
+        </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

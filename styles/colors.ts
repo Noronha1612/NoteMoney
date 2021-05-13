fc00574440112f6c3bd0c800aca2457a066e228c
NoronhaGCP <inc.691@gmail.com>
2021-05-13 17:56:39 -0300

    create styles variables

---
+++ styles/colors.ts
@@ -0,0 +1,6 @@
+export const colors = {
+    background: '#2F3640',
+    green: '#44BD32',
+    red: '#E84118',
+    yellow: '#FBC531',
+}
---
+++ styles/fonts.ts
@@ -0,0 +1,7 @@
+export const fonts = {
+    light: 'Roboto_300Light',
+    regular: 'Roboto_400Regular',
+    italic: 'Roboto_400Regular_Italic',
+    medium: 'Roboto_500Medium',
+    bold: 'Roboto_700Bold'
+}

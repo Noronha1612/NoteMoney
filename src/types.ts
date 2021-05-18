97ecb902ac6b9102caf68dd9904791f89aead9d5
NoronhaGCP <inc.691@gmail.com>
2021-05-18 17:35:57 -0300

    update ICard types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
+export type ICardType = 'm-revenue' | 'a-revenue' | 'saving' | '';
+
 export interface ICard {
     title: string;
     currentValue: number;
     referralValue: number;
+    type: ICardType;
+    replenishmentDay?: number;
 }

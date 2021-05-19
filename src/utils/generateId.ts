a9d7e6b6e15e8a666432ed1cd4f28ed2574b2d71
NoronhaGCP <inc.691@gmail.com>
2021-05-19 16:48:23 -0300

    add validation to generateId

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -1,9 +1,24 @@
 import store from '../store';
 
 export default function generateId(): number {
+    let id: number;
+
     const { cards } = store.getState();
 
-    if ( cards.length === 0 ) return 1;
+    if ( cards.length === 0 ) id = 1;
+    else id = cards[cards.length - 1].id + 1;
 
-    return cards[cards.length - 1].id + 1;
+    while(true) {
+        let idAlreadyExist = false;
+        
+        cards.forEach(card => {
+            if ( card.id === id ) idAlreadyExist = true;
+        });
+
+        if (!idAlreadyExist) break;
+
+        id++;
+    }
+
+    return id;
 }

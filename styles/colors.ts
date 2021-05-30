d84365eedae18f2fb11ae89685349e41131cb36d
NoronhaGCP <inc.691@gmail.com>
2021-05-30 15:55:52 -0300

    add red transparent color

--- styles/colors.ts
+++ styles/colors.ts
@@ -3,6 +3,7 @@ export const colors = {
     green: '#44BD32',
     greenTransparent: 'rgba(68, 189, 50, 0.6)',
     red: '#E84118',
+    redTransparent: '#E8411860',
     yellow: '#FBC531',
     shape: '#718093',
     textGreen: '#009432',
